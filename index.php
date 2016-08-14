d808f4f1874d089fe6ab77803157bb3db37d7c8e
DougHucker <doug@vitaminisgood.com>
2016-08-14 11:57:03 -0400

    Doug Test Branch

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<?php echo "<h1>Hello Git!</h1>"; ?>
-<?php echo "<h1>WHY SO CONFUSING?</h1>"; ?>
-<?php echo "<h1>I hate this</h1>"; ?>
-<<<<<<< HEAD
-<p>Testing edit again without syncing</p>
-=======
-<p>Testing edit</p>
-<p>Testing edit 2</p>
->>>>>>> origin/master
+<h1>Doug test branch commit.</h1>
