2d928edb47a567aea893c590577e34860c858640
Chris Gilbert <chris_v>
2016-08-14 11:48:58 -0400

    testing conflict

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php echo "<h1>Hello Git!</h1>"; ?>
 <?php echo "<h1>WHY SO CONFUSING?</h1>"; ?>
 <?php echo "<h1>I hate this</h1>"; ?>
-<p>Testing edit</p>
+<p>Testing edit again without syncing</p>
